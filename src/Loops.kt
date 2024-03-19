5d022d89ff5c45801fc3ff4808ecb4666fb71dfc
iMoregano007 <imgauravs84@gmail.com>
2024-03-19 15:19:59 +0530

    Kotlin Journey Begins Day - 1.2

---
+++ src/Loops.kt
@@ -0,0 +1,29 @@
+fun main(){
+//    print 2's table using while loop
+    val num = 2
+    var index = 1
+
+    println( "print 2's table using while loop")
+
+
+    while(index <= 10){
+        println("" + num +" * " + index + " = " + num*index++)
+    }
+    index = 1
+//     print 2's table using do while loop
+    println( "print 2's table using do while loop")
+    do{
+        println("" + num +" * " + index + " = " + num*index++)
+
+    } while(index <= 10)
+
+//    for loop
+    println( "print 2's table using for loop")
+
+    var i = 1
+    index = i
+    for(i in 1..10){
+        println("" + num +" * " + index + " = " + num*index++)
+
+    }
+}
