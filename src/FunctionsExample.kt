facd9fd56203bc4ce5d6cc2dc00f3bd0d468d16d
iMoregano007 <imgauravs84@gmail.com>
2024-03-28 12:26:41 +0530

    Day 3 : Method overloading

--- src/FunctionsExample.kt
+++ src/FunctionsExample.kt
@@ -1,32 +1,38 @@
 fun main(){
-    println("Normal function")
-    println(add(55,5))
+//    named argument
+    println(add(a=5,b=8))
+    println(add(b=2.0,a=5.0))
+    println(power(2.0,3.0))
+    println(power(b = 2.0,a=3.0))
 
-    println("One Liner function")
-    println(addOneLinerF(55,5))
-
-    println("Default Argument function")
-    println(addDefault())
-
-    println("Unit function")
-    unitAddF(55,5)
 }
+//
+////normal function
+//fun add(num1: Int, num2: Int) : Int{
+//    return num1 + num2
+//}
+//// one Liner function
+//fun addOneLinerF(num1: Int , num2: Int): Int = num1 + num2
+//
+////default argument func
+//
+//fun addDefault(num1: Int =2, num2: Int = 3): Int {
+//    return num1+num2
+//}
+//
+////unit func
+//
+//fun unitAddF(num1: Int, num2: Int){
+//    println(num1+num2)
+//}
 
-//normal function
-fun add(num1: Int, num2: Int) : Int{
-    return num1 + num2
-}
-// one Liner function
-fun addOneLinerF(num1: Int , num2: Int): Int = num1 + num2
 
-//default argument func
+//function overloading
 
-fun addDefault(num1: Int =2, num2: Int = 3): Int {
-    return num1+num2
-}
+fun add(a: Int, b: Int): Int = a+b
+fun add(a: Double , b: Double): Double = a+b
 
-//unit func
-
-fun unitAddF(num1: Int, num2: Int){
-    println(num1+num2)
+fun power(a: Double, b: Double):Double
+{
+    return Math.pow(a,b)
 }
